3e123da67b3cd39e690ffd7a24d8ce0484ad7f3d
Владислав Симонов <simonov.2019@azure.nstu.ru>
2021-04-07 10:18:06 +0700

    lw2

--- main.cpp
+++ main.cpp
@@ -5,33 +5,31 @@ struct treap
 	treap* left;
 	treap* right;
 	std::pair<int, int> key_priority;
-	treap(int x, int y, treap* left = nullptr, treap* right = nullptr)
+	treap(const int& x, const int& y, treap* left = nullptr, treap* right = nullptr)
 	{
 		this->left = left;
 		this->right = right;
-		key_priority = std::pair<int, int>(x, y);
+		key_priority = std::make_pair(x, y);
 	}
 
-	std::pair<treap*, treap*> split(treap* treap, int key)
+	std::pair<treap*, treap*> split(treap* treap, const int& key)
 	{
 		if (treap == nullptr)
-			return std::pair<::treap*, ::treap*>(nullptr, nullptr);
+			return std::make_pair(nullptr, nullptr);
 
 		else if (key > treap->key_priority.first)
 		{
 			std::pair<::treap*, ::treap*> pair_treap = split(treap->right, key);
 			treap->right = pair_treap.first;
-			return std::pair<::treap*, ::treap*>(treap, pair_treap.second);
+			return std::make_pair(treap, pair_treap.second);
 		}
 
 		else
 		{
 			std::pair<::treap*, ::treap*> pair_treap = split(treap->left, key);
 			treap->left = pair_treap.second;
-			return std::pair<::treap*, ::treap*>(pair_treap.first, treap);
-		}
-
-		
+			return std::make_pair(pair_treap.first, treap);
+		}		
 	}
 };
 
